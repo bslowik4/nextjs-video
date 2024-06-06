7418b994429b8abd3cd30bcf2fb179a45ac2c87e
bslowik4 <bslowik4@outlook.com>
2024-06-06 22:06:31 +0200

    Adding text to image
    Code for adding text to image

--- nextjs-video/app/ui/edit/imageInput.tsx
+++ nextjs-video/app/ui/edit/imageInput.tsx
@@ -3,15 +3,39 @@ import { useState } from "react"
 
 export default function  ImageInput(){
     const [selectedImage, setSelectedImage] = useState("");
-    function handleChange(e) {
+    const [writtenText, setWrittenText] = useState("");
+    function handleImage(e: React.ChangeEvent<HTMLInputElement>): void {
         console.log(e.target.files);
         setSelectedImage(URL.createObjectURL(e.target.files[0]));
     }
-    
+    function handleText(e: React.ChangeEvent<HTMLInputElement>): void {
+        setWrittenText(e.target.value);
+        let img = new Image();
+
+        img.onload = () => {
+            let canva = document.createElement("canvas");
+            let ctx = canva.getContext("2d");
+
+            canva.width = img.naturalWidth;
+            canva.height = img.naturalHeight;
+            ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight);
+
+            ctx.font = "bold 20px Arial";
+            ctx.fillStyle = "rgb(0, 0, 0)";
+            ctx.fillText(`${writtenText}`, 60, 80);
+            document.body.appendChild(canva);
+
+            }
+            img.src = `${selectedImage}`;
+            
+
+    } 
+
     return(
         <>  
-        <input type="file" onChange={handleChange} />
+        <input type="file" onChange={handleImage} />
         <img src={selectedImage} alt={selectedImage}></img>
+        <input type="text" onChange={handleText} className="text-black"/>
         </>
     )
 }
