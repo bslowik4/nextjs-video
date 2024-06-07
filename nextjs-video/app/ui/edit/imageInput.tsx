5bcc6faef270c0e6a520bd5fd63fc3dde58d38d9
bslowik4 <bslowik4@outlook.com>
2024-06-07 11:53:26 +0200

    Updated image text
    Moved text adding to button click from state changed.

--- nextjs-video/app/ui/edit/imageInput.tsx
+++ nextjs-video/app/ui/edit/imageInput.tsx
@@ -2,20 +2,19 @@
 import { useState } from "react"
 
 export default function  ImageInput(){
-    document.body.appendChild(document.createElement("canvas"));
-
     const [selectedImage, setSelectedImage] = useState("");
     const [writtenText, setWrittenText] = useState("");
+    const [imageWithText, setImageWithText] = useState("");
+
     function handleImage(e: React.ChangeEvent<HTMLInputElement>): void {
         console.log(e.target.files);
         setSelectedImage(URL.createObjectURL(e.target.files[0]));
     }
-
     function handleText(e: React.ChangeEvent<HTMLInputElement>): void {
-        document.body.removeChild(document.querySelector("canvas"));
         setWrittenText(e.target.value);
+    } 
+    function addTextToimage(){
         let img = new Image();
-
         img.onload = () => {
             let canva = document.createElement("canvas");
             let ctx = canva.getContext("2d");
@@ -27,17 +26,20 @@ export default function  ImageInput(){
             ctx.font = "bold 20px Arial";
             ctx.fillStyle = "rgb(0, 0, 0)";
             ctx.fillText(`${writtenText}`, 60, 80);
-            document.body.appendChild(canva);
+            
+            setImageWithText(canva.toDataURL('image/png')); 
 
             }
             img.src = `${selectedImage}`;
-    } 
+    }
 
     return(
         <>  
         <input type="file" onChange={handleImage} />
         <img src={selectedImage} alt={selectedImage}></img>
         <input type="text" onChange={handleText} className="text-black"/>
+        <button type="button" onClick={addTextToimage}>Add text to image</button>
+        <img src={imageWithText} alt={imageWithText}></img>
         </>
     )
 }
