05c5c7a826bb5666e1606f8433f9fd77b01f100e
bslowik4 <bslowik4@outlook.com>
2024-06-07 12:02:17 +0200

    Fixed typescript issues
    added null check to not execute code when variable is null

--- nextjs-video/app/ui/edit/imageInput.tsx
+++ nextjs-video/app/ui/edit/imageInput.tsx
@@ -7,30 +7,34 @@ export default function  ImageInput(){
     const [imageWithText, setImageWithText] = useState("");
 
     function handleImage(e: React.ChangeEvent<HTMLInputElement>): void {
-        console.log(e.target.files);
-        setSelectedImage(URL.createObjectURL(e.target.files[0]));
+        if (e.target.files) {
+            setSelectedImage(URL.createObjectURL(e.target.files[0]));
+        }
     }
+
     function handleText(e: React.ChangeEvent<HTMLInputElement>): void {
         setWrittenText(e.target.value);
     } 
-    function addTextToimage(){
+    
+    function addTextToImage() {
         let img = new Image();
         img.onload = () => {
             let canva = document.createElement("canvas");
             let ctx = canva.getContext("2d");
-
-            canva.width = img.naturalWidth;
-            canva.height = img.naturalHeight;
-            ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight);
-
-            ctx.font = "bold 20px Arial";
-            ctx.fillStyle = "rgb(0, 0, 0)";
-            ctx.fillText(`${writtenText}`, 60, 80);
-            
-            setImageWithText(canva.toDataURL('image/png')); 
-
+    
+            if (ctx) {
+                canva.width = img.naturalWidth;
+                canva.height = img.naturalHeight;
+                ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight);
+    
+                ctx.font = "bold 20px Arial";
+                ctx.fillStyle = "rgb(0, 0, 0)";
+                ctx.fillText(`${writtenText}`, 60, 80);
+    
+                setImageWithText(canva.toDataURL('image/png'));
             }
-            img.src = `${selectedImage}`;
+        };
+        img.src = `${selectedImage}`;
     }
 
     return(
@@ -38,7 +42,7 @@ export default function  ImageInput(){
         <input type="file" onChange={handleImage} />
         <img src={selectedImage} alt={selectedImage}></img>
         <input type="text" onChange={handleText} className="text-black"/>
-        <button type="button" onClick={addTextToimage}>Add text to image</button>
+        <button type="button" onClick={addTextToImage}>Add text to image</button>
         <img src={imageWithText} alt={imageWithText}></img>
         </>
     )
