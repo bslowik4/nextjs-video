ea5a0417595fe0d916bbed037fb8a6366a5f1c87
bslowik4 <bslowik4@outlook.com>
2024-07-01 14:21:23 +0200

    Removed unused editData
    removed unused fetch for now

--- nextjs-video/app/edit/page.tsx
+++ nextjs-video/app/edit/page.tsx
@@ -1,28 +1,8 @@
 import ImageInput  from '@/app/ui/edit/imageInput';
-import { useEffect, useState } from 'react';
-import fetcher from '../utils/fetch';
-import { getToken } from '../utils/auth';
 import ProtectedRoute from '../ui/edit/protectedRout';
 import Layout from '@/app/layout';
 
 const EditPage: React.FC = () => {
-  const [editData, setEditData] = useState<any[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const token = getToken();
-        if (token) {
-          const data = await fetcher('/api/edit', token);
-          setEditData(data);
-        }
-      } catch (err) {
-        console.error('Error fetching data:', err);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   return (
     <ProtectedRoute>
--- nextjs-video/app/ui/edit/protectedRout.tsx
+++ nextjs-video/app/ui/edit/protectedRout.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useEffect, useState } from 'react';
 import { redirect } from 'next/navigation';
 import { getToken } from '@/app/utils/auth';
